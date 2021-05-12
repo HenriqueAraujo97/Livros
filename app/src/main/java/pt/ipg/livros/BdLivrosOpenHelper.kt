c3fcab497016d95826f7962bffaced562f871643
HenriqueAraujo97 <araujo.henrique311@gmail.com>
2021-05-12 12:38:20 +0100

    Criada a class para apresentar a tabela de categorias

--- app/src/main/java/pt/ipg/livros/BdLivrosOpenHelper.kt
+++ app/src/main/java/pt/ipg/livros/BdLivrosOpenHelper.kt
@@ -6,11 +6,14 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class BdLivrosOpenHelper(context: Context?) : SQLiteOpenHelper(context, NOME_BASE_DADOS, null, VERSAO_BASE_DADOS) {
-    override fun onCreate(p0: SQLiteDatabase?) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        if (db != null) {
+            TabelaCategorias(db).cria()
+        }
 
     }
 
-    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
 
     }
 
