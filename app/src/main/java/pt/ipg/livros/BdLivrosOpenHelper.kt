d0ef8c5ed23c2f7db8ec38558a6da60d67d9531d
HenriqueAraujo97 <araujo.henrique311@gmail.com>
2021-05-12 12:23:08 +0100

    Criada a class para abrir a base de dados de livros

--- app/src/main/java/pt/ipg/livros/BdLivrosOpenHelper.kt
+++ app/src/main/java/pt/ipg/livros/BdLivrosOpenHelper.kt
@@ -1,2 +1,22 @@
 package pt.ipg.livros
 
+import android.content.Context
+import android.database.DatabaseErrorHandler
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class BdLivrosOpenHelper(context: Context?) : SQLiteOpenHelper(context, NOME_BASE_DADOS, null, VERSAO_BASE_DADOS) {
+    override fun onCreate(p0: SQLiteDatabase?) {
+
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+
+    }
+
+    companion object{
+        const val NOME_BASE_DADOS = "Livros.db"
+        const val VERSAO_BASE_DADOS = 1
+    }
+
+}
