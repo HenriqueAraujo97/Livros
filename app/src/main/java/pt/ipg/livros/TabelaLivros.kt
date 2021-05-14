4d1fd00c03e047433b54f5a415762da99922f6ca
HenriqueAraujo97 <araujo.henrique311@gmail.com>
2021-05-14 11:27:24 +0100

    Adicionada as constantes das tabelas

--- app/src/main/java/pt/ipg/livros/TabelaLivros.kt
+++ app/src/main/java/pt/ipg/livros/TabelaLivros.kt
@@ -1,11 +1,25 @@
 package pt.ipg.livros
 
 import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
 
 class TabelaLivros(db: SQLiteDatabase) {
     private val db: SQLiteDatabase = db
 
     fun cria() {
-        db.execSQL("CREATE TABLE livros(_id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT NOT NULL, autor NOT NULL, id_categoria INTEGER NOT NULL, FOREIGN KEY(id_categoria) REFERENCES categorias) ")
+        db.execSQL("CREATE TABLE " + NOME_TABLEA+ "(" + BaseColumns._ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                CAMPO_TITULO + " TEXT NOT NULL, " +
+                CAMPO_AUTOR + " TEXT NOT NULL, " +
+                CAMPO_ID_CATEGORIA + " INTEGER NOT NULL, FOREIGN KEY( " +
+                CAMPO_ID_CATEGORIA + ") REFERENCES " +
+                TabelaCategorias.NOME_TABELA +
+                ") ")
+    }
+
+    companion object{
+        const val NOME_TABLEA = "livros"
+        const val  CAMPO_TITULO = "titulo"
+        const val CAMPO_AUTOR = "autor"
+        const val CAMPO_ID_CATEGORIA = "id_categoria"
     }
 }
